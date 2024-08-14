2aaef17a30f1226c43b87400de10d845d46d7f15
Vicieta31 <danicastillor04@gmail.com>
2024-08-14 18:01:10 +0200

    Walk Animations

--- BossCard/Assets/Scripts/playerScript.cs
+++ BossCard/Assets/Scripts/playerScript.cs
@@ -6,10 +6,20 @@ public class player : MonoBehaviour
 {
 
     public float speed;
-    float xMove = 0;
-    float YMove = 0;
-
+    private Vector2 moveInp;
+        
     Rigidbody2D rb;
+    bool mRight;
+    bool mLeft;
+    bool mUp;
+    bool mDown;
+
+    int playerDir;
+
+    public Animator playerAnimator;
+
+    int lastDir = 0;
+
 
     // Start is called before the first frame update
     void Start()
@@ -20,19 +30,102 @@ public class player : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        UpdateInput();
+        UpdateMovemnt();
+        checkDirection();
         UpdateDirVel();
     }
 
-    void UpdateInput()
+    void UpdateMovemnt()
     {
-        xMove = Input.GetAxis("Horizontal");
-        YMove = Input.GetAxis("Vertical");
+        moveInp.x = Input.GetAxisRaw("Horizontal");
+        moveInp.y = Input.GetAxisRaw("Vertical");
+
+        moveInp.Normalize();
+
     }
     void UpdateDirVel()
     {
-        Vector2 dir = new Vector2(xMove, YMove).normalized;
-
-        rb.velocity = dir * speed;
+        rb.velocity = moveInp * speed;
     }
+
+    void checkDirection()
+    {
+        if ((moveInp.x == 0) && (moveInp.y == 0))
+        {
+            if(lastDir == 1)
+            {
+                playerAnimator.Play("idleRight");
+            }
+            if (lastDir == 2)
+            {
+                playerAnimator.Play("idleLeft");
+            }
+            if (lastDir == 0)
+            {
+                playerAnimator.Play("idleMe");
+            }
+            if (lastDir == 3)
+            {
+                playerAnimator.Play("idleUp");
+            }
+                        
+        }
+        if (moveInp.x > 0)
+        {
+            if (mUp != true && mDown != true)
+            {
+                mRight = true;
+                lastDir = 1;
+                playerAnimator.Play("walkRMe");
+            }
+        }
+
+        else
+        {
+            mRight = false;
+        }
+        if (moveInp.x < 0)
+        {
+            if (mUp != true && mDown != true)
+            {
+                lastDir = 2;
+                mLeft = true;
+                playerAnimator.Play("walkLMe");
+            }
+            
+        }
+        else
+        {
+            mLeft = false;
+        }
+
+        if (moveInp.y > 0)
+        {
+            if (mLeft != true && mRight != true)
+            {
+                lastDir = 3;
+                mUp = true;
+                playerAnimator.Play("walkUMe");
+            }
+        }
+        else
+        {
+            mUp = false;
+        }
+
+        if (moveInp.y < 0)
+        {
+            if (mLeft != true && mRight != true)
+            {
+                lastDir = 0;
+                mDown = true;
+                playerAnimator.Play("walkDMe");
+            }
+        }
+        else
+        {
+            mDown = false;
+        }
+    }
+
 }
